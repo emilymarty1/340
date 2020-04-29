9e11031cacf4d54a5e22f24bdf9e330585b4f366
emilymarty1 <60709578+emilymarty1@users.noreply.github.com>
2020-04-29 16:06:35 +0000

    Sixth and Seventh Projects

---
+++ MySeventhProject/App copy.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+
+      <View style= {styles.containerButton}>
+        <Button
+          onPress={() => {
+            alert('You tapped button 1');
+            }}
+          title='Press Button 1'
+      />
+    </View>
+    
+    <View style= {styles.ContainerLayoutButton}>
+   
+    <Button
+          onPress={() => {
+            alert('You tapped button 2');
+            }}
+          title='Press Button 2'
+      />
+
+    <Button
+          onPress={() => {
+            alert('You tapped Great');
+            }}
+          title='Great'
+          color='green'
+      />
+    
+    </View>
+    
+    <View style={styles.containerButton}>
+    
+    <Button
+          onPress={() => {
+            alert('You tapped button Tab Me');
+            }}
+          title='Tap Me'
+          
+      /> 
+
+    </View>
+
+
+
+    </View>
+
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+
+  containerButton: {
+    margin: 25,
+  },
+
+  containerLayoutButton: {
+    margin:20,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  }
+
+});
